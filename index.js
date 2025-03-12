841147e6c5af4ff373e46bd712c8a576441d8475
simplykevine <kevineumutonibugingo@gmail.com>
2025-03-12 12:03:58 +0300

    javascript

---
+++ index.js
@@ -0,0 +1,19 @@
+let name="Kevine"
+console.log(name)
+let cohort="Lovelace"
+console.log(cohort)
+cohort="AnitaB"
+console.log(cohort)
+cohort="Ada lab"
+console.log(cohort)
+console.log(2, "ABC", 3*5, "ABCDEF", 9*9)
+console.log("the output of 32 is 10+11 ", 21)
+console.log("Hello "+"I am Kevine "+ "I love myself " + "I believe in myself "+ "and God. ")
+console.log(name + " is very beautiful")
+// practicing numbers and integers in data types in JS
+let y = 1.2;
+let x= 5;
+console.log(x+y)
+// practicing booleans in Javascript
+let n=true;
+let f= false;
