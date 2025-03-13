d2b3471ac13a05d76fe2f977a4cdcca285123d09
simplykevine <kevineumutonibugingo@gmail.com>
2025-03-13 09:44:51 +0300

    javascript

--- index.js
+++ index.js
@@ -17,3 +17,69 @@ console.log(x+y)
 // practicing booleans in Javascript
 let n=true;
 let f= false;
+console.log(n)
+console.log(f)
+//Let's try to convert from string to Int
+let i="5.9";
+let p=7
+c=Number(i)
+console.log(c)
+//Let's try adding int and string
+r= "34"
+h=12
+console.log(r+h)
+q=[1,2,4,7,+ 3,4,6,7]
+m="9,8,7"
+console.log(q+m)
+//Let's try to add String and Bool
+x=true+" 12"
+console.log(x)
+d= false+" 23"
+console.log(d)
+u="10 "+ false
+console.log(u)
+s="98 " + true
+console.log(s)
+//lets try adding bool to Integers
+e= 34;
+g= true;
+o=(e+g)
+console.log(o)//the output is 35 because true means 1;
+y= 56
+b= false
+w=(y+b)
+console.log(w)//The output was 56 because false means 0
+
+
+//let me practice addition.
+j= 6;
+l=56
+console.log(j+l)  //the output is 35
+
+//Let me practice subtraction
+n= 98;
+k= 56;
+b=(n-k)
+console.log(b) //the output is 56
+
+//let me use var
+
+var v=7
+var w=45 
+console.log(v+w)//the output was 52
+
+//let me practice importing a valuable
+let kevine="Umutoni"
+var age=21
+console.log(`My name is ${kevine} I am ${age}`)
+//let me practice adding numbers in square brackets and not in square brackets
+var fh= [1,23]
+var u=67
+console.log(fh+u)
+//experimentation
+var num = 23
+console.log(num)
+var mun = "23"
+console.log(mun)
+console.log(typeof(mun))
+console.log(typeof(num))
